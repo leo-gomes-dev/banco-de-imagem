4d12310059e7bc210e2018fa997c9af6cda475d7
Leogom26 <leogomecommerce@gmail.com>
2026-02-20 15:50:51 -0300

    atualizado pastas, moto, barbearia e aleatorio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,20 +7,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br" className="dark">
+    // Adicione suppressHydrationWarning aqui
+    <html lang="pt-br" className="dark" suppressHydrationWarning>
       <head>
-        {/* Anti-Flash: Força a cor de fundo no carregamento inicial */}
         <meta name="theme-color" content="#18181b" />
       </head>
-      {/* 
-          flex flex-col + min-h-screen: Empurra o footer para o final.
-          m-0 p-0: Remove espaços que criam a "faixa preta".
-      */}
       <body className="bg-[#18181b] text-white flex flex-col min-h-screen m-0 p-0">
-        {/* O flex-grow faz o conteúdo principal "esticar" até o footer */}
         <main className="flex-grow w-full">{children}</main>
-
-        {/* Footer integrado no fluxo, sem gap e sem bordas gritantes */}
         <Footer />
       </body>
     </html>
